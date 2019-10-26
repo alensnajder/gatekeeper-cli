8d0a1bf01157094b5d538604b386a6134e8c5597
Alen Snajder <alen.snajder@gmail.com>
2019-10-26 13:36:01 +0200

    Initial project setup

---
+++ gatekeeper.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from __future__ import division
+import RPi.GPIO as GPIO
+import argparse
+import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-p", "--pin", type=int, help="GPIO BCM Pin number", required=True)
+parser.add_argument("-d", "--duration", type=int, help="Duration of toggle", required=True)
+
+args = parser.parse_args()
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(args.pin, GPIO.OUT)
+
+GPIO.output(args.pin, GPIO.HIGH)
+time.sleep(args.duration / 1000)
+GPIO.output(args.pin, GPIO.LOW)
+GPIO.cleanup()
